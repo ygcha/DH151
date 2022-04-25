b60794e9a89122b9ece640f2f1a73af7eef525ea
Christina Cha <ygcha76@g.ucla.edu>
2022-04-24 18:18:21 -0700

    hw

---
+++ Week4/js/cmap.js
@@ -0,0 +1,75 @@
+// Global variables
+let map;
+let lat = 0;
+let lon = 0;
+let zl = 1;
+
+// path to csv data
+let path = "data/chipotle_store.csv";
+
+let markers = L.featureGroup();
+
+
+// initialize
+$( document ).ready(function() {
+    createMap(lat,lon,zl);
+    readCSV(path);
+});
+
+// create the map
+function createMap(lat,lon,zl){
+	map = L.map('map').setView([lat,lon], zl);
+
+	L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+function flyToIndex(lat, lon){
+	map.flyTo([lat,lon],12)
+};
+
+// function to read csv data
+function readCSV(path){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			
+			// map the data
+			mapCSV(data);
+
+		}
+	});
+}
+
+function mapCSV(data){
+	
+	// circle options
+	let circleOptions = {
+		radius: 5,
+		weight: 1,
+		color: 'white',
+		fillColor: 'red',
+		fillOpacity: 1
+	}
+
+	// loop through each entry
+	data.data.forEach(function(item,index){
+		// create marker
+		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+		.on('mouseover', function(){
+			this.bindPopup(`${item.date}<br>${item.desc}`).openPopup()
+		})
+
+		// add marker to featuregroup		
+		markers.addLayer(marker)
+	})
+
+	// add featuregroup to map
+	markers.addTo(map)
+
+	// fit markers to map
+	map.fitBounds(markers.getBounds())
+}
